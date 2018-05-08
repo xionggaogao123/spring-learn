f471d37b8a056d3b7295b7d94c3264bff25b36e6
xionggao <xionggao@xionggaodeMacBook-Air.local>
2018-05-08 18:12:19 +0800

    init

---
+++ readme.md
@@ -0,0 +1,12 @@
+
+重温 spring 
+
+（1）spring 创建 bean 的几种方式
+
+（2）spring 启动流程
+
+（3）spring ioc 是怎么创建bean 
+
+（4）spring aop
+
+ 
---
+++ src/main/java/com/keven/spring/learn/test/TestController.java
@@ -0,0 +1,17 @@
+package com.keven.spring.learn.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+
+        return "hello world";
+    }
+}
